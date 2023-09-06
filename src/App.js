5c1bb09d3274af5389b9b5eca0caadc1df6354b5
agorbacheva-y <agorbacheva.y@gmail.com>
2023-09-06 21:20:00 +0200

    spacing of Layout components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Layout from './components/Layout';
 
 function App() {
   return (
-    <div>
+    <div className='body-container'>
       <Layout style={{backgroundColor: "hsl(10, 79%, 65%)", color: "white"}}>
         <h1>My Balance</h1>
       </Layout>
