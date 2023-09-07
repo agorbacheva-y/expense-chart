63fbabb5a84c69ac31e6847526b43c284e32831f
agorbacheva-y <agorbacheva.y@gmail.com>
2023-09-07 22:13:16 +0200

    use async await for fetching data

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -3,18 +3,31 @@ import { useState, useEffect } from "react";
 function Chart() {
   const [ chartData, setChartData ] = useState([]);
 
+  // useEffect(() => {
+  //   fetch("data.json")
+  //   .then(res => res.json())
+  //   .then(data => setChartData(data))
+
+  //   console.log(chartData);
+  // },[]);
+
   useEffect(() => {
-    fetch("data.json")
-    .then(res => res.json())
-    .then(data => setChartData(data))
+    fetchChartData();
   },[]);
 
-  //console.log(chartData[0].amount);
+  const fetchChartData = async () => {
+    const response = await fetch("/data.json");
+    const data = await response.json();
+
+    setChartData(data.map(({ amount }) => amount));
+  };
+
+  console.log(chartData);
 
   return (
     <div className='chart'>
       <div className='chart__bar'>
-          <div className='chart__fill' style={{ height: `${chartData[3].amount}%` }}></div>
+          <div className='chart__fill' style={{ height: `${chartData}%` }}></div>
       </div>
     </div>
   );
