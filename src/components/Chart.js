e9e9c9dcda39092de98241c7c356f8fea8a1be87
agorbacheva-y <agorbacheva.y@gmail.com>
2023-09-07 21:51:49 +0200

    fetch json data

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,4 +1,16 @@
+import { useState, useEffect } from "react";
+
 function Chart() {
+  const [ chartData, setChartData ] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+    .then(res => res.json())
+    .then(data => setChartData(data))
+  },[]);
+
+  console.log(chartData);
+
   return (
     <div className='chart'>
       <div className='chart__bar'>
