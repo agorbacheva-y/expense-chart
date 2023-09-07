b294efa18d37d4bfb297914a152dd91c163c9b7a
agorbacheva-y <agorbacheva.y@gmail.com>
2023-09-07 21:42:43 +0200

    static bar chart

---
+++ src/components/Chart.js
@@ -0,0 +1,11 @@
+function Chart() {
+  return (
+    <div className='chart'>
+      <div className='chart__bar'>
+        <div className='chart__fill'></div>
+      </div>
+    </div>
+  );
+};
+
+export default Chart;
--- src/components/Spending.js
+++ src/components/Spending.js
@@ -1,10 +1,12 @@
 import Layout from './Layout';
+import Chart from './Chart';
 
 function Spending() {
   return (
     <div className='spending'>
       <Layout style={{backgroundColor: "white", color: "hsl(25, 47%, 15%)"}}>
         <h3>Spending - Last 7 days</h3>
+        <Chart />
         <hr />
         <p>Total this month</p>
         <h2>balance</h2>
