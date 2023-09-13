58d0517a5d4bdd8feeea34e9096a84946a01552b
agorbacheva-y <agorbacheva.y@gmail.com>
2023-09-13 14:46:54 +0200

    hover effect in jsx

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,15 +1,20 @@
 import { useState, useEffect } from "react";
 
 function Chart({ chartDate }) {
-  // const [ chartAmount, setChartAmount ] = useState([]);
+  const [ chartAmount, setChartAmount ] = useState([]);
   const [ dayIndex, setDayIndex ] = useState(0);
   const [ spendingAmount, setSpendingAmout ] = useState([]);
+  const [ isHover, setIsHover ] = useState(false);
   let day = 'wed';
   
   useEffect(() => {
     fetchSpendingData();
     currentDay();
   },[]);  
+
+  useEffect(() => {
+    fetchChartData();
+  },[spendingAmount])
 
   // get amount and date from data.json
   const fetchSpendingData = async () => {
@@ -19,16 +24,11 @@ function Chart({ chartDate }) {
     setSpendingAmout(data.map(({ amount }) => amount));
   };
 
-  // useEffect(() => {
-  //   fetchChartData();
-  //   currentDay();
-  // },);
-
-  // // adjust heights so max y value is 70%
-  // const fetchChartData = () => {
-  //   const newAmount = spendingAmount.map(currentValue => currentValue/0.7);
-  //   setChartAmount(newAmount);
-  // };
+  // adjust heights so max y value is 70%
+  const fetchChartData = () => {
+    const newAmount = spendingAmount.map(currentValue => currentValue/0.7);
+    setChartAmount(newAmount);
+  };
 
   // set index of current day
   const currentDay = () => { 
@@ -61,25 +61,37 @@ function Chart({ chartDate }) {
   };
   //console.log(dayIndex);
 
+  const handleMouseOver = () => {
+    setIsHover(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHover(false);
+  };
+
   return (
     <div className="chart">
-    
-      <div className="chart__bar">
-        {spendingAmount.map((item, id) => (
-          <div key={id} className="chart__overlay">${item}</div>
-        ))}
-      </div>
-      
-      <div className="chart__fill">
-        {spendingAmount.map((item, id) => (
+
+      <div className="chart__fill" >
+        {chartAmount.map((item, id) => (
           <div
             key={id}
+            onMouseOver={handleMouseOver} 
+            onMouseOut={handleMouseOut}
             className={dayIndex === id ? 'chart__fill--today' : 'chart__fill--other'}
             style={{ height: `${item}%` }}
           >
           </div>
         ))}
       </div>
+
+      {isHover && (
+        <div className="chart__bar">
+        {spendingAmount.map((item, id) => (
+          <div key={id} className="chart__overlay">${item}</div>
+        ))}
+        </div>
+      )}
 
       <div className="chart__week">
         {chartDate.map((item, id) => (
